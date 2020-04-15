48647b1ecddd6a752a4abd4d84c21d333583c750
Marcus Russi <marcusrussi@gmail.com>
2020-04-15 18:04:25 -0400

    Tidied things up

--- R/priors.R
+++ R/priors.R
@@ -59,13 +59,13 @@ priors_progression <- function(...) {
   splice_priors(defaults, args)
 }
 
-# inf to res not well supported in data, guess:           
-# assume mean 10 days, CI 5, 13                           
-# based on 5 days inf -> sym, 5 days sym -> res (CI 2,8)  
-# adjust later?                                           
+# inf to res not well supported in data, guess:
+# assume mean 10 days, CI 5, 13
+# based on 5 days inf -> sym, 5 days sym -> res (CI 2,8)
+# adjust later?
 #
 
-#' Priors on delay to recovered                                      
+#' Priors on delay to recovered
 #'
 #' Slightly longer description
 #'
@@ -77,12 +77,12 @@ priors_recovery <- function(...) {
   args <- list(...)
 
   list(
-    inf_res_delay_shap = 23.83,                               
-    inf_res_delay_rate = 2.383,                               
-    sym_res_delay_shap = 10.50,                               
-    sym_res_delay_rate = 2.099,                               
-    hos_res_delay_shap = 60.86,                               
-    hos_res_delay_rate = 3.567                                
+    inf_res_delay_shap = 23.83,
+    inf_res_delay_rate = 2.383,
+    sym_res_delay_shap = 10.50,
+    sym_res_delay_rate = 2.099,
+    hos_res_delay_shap = 60.86,
+    hos_res_delay_rate = 3.567
   ) -> defaults
 
   att(all(args %in% defaults))
@@ -90,7 +90,7 @@ priors_recovery <- function(...) {
   splice_priors(defaults, args)
 }
 
-#' Priors on reporting delay: assumed                                    
+#' Priors on reporting delay: assumed
 #'
 #' Slightly longer description
 #'
@@ -102,7 +102,7 @@ priors_reporting_delay <- function(...) {
   args <- list(...)
 
   list(
-    pri_report_delay_shap = 7,                                 
+    pri_report_delay_shap = 7,
     pri_report_delay_rate = 0.9
   ) -> defaults
 
@@ -122,12 +122,12 @@ priors_diagnosis <- function(...) {
   args <- list(...)
 
   list(
-    pri_p_diag_if_inf_a = 1,                                   
-    pri_p_diag_if_inf_b = 99,                                  
-    pri_p_diag_if_sym_a = 60,                                  
-    pri_p_diag_if_sym_b = 40,                                  
-    pri_p_diag_if_hos_a = 95,                                  
-    pri_p_diag_if_hos_b = 5                                    
+    pri_p_diag_if_inf_a = 1,
+    pri_p_diag_if_inf_b = 99,
+    pri_p_diag_if_sym_a = 60,
+    pri_p_diag_if_sym_b = 40,
+    pri_p_diag_if_hos_a = 95,
+    pri_p_diag_if_hos_b = 5
   ) -> defaults
 
   att(all(args %in% defaults))
@@ -173,26 +173,26 @@ genData <- function(diagData)
   rlang::dots_list(
     .homonyms = "error", # Ensure that no keys are entered twice
 
-    N_conf_cases = length(diagData$diagnosis_day),                     # !!
-                                                                       # !!
-    #n days to model before first case                                 # !!
-    N_days = N_days,                                                   # !!
-                                                                       # !!
-    # model days after (for things like death, hospitalizations)       # !!
-    N_days_extra = N_days_extra,                                       # !!
-                                                                       # !!
-    # maximum value for delay distribution                             # !!
-    Max_delay = max(diagData$days_delay),                              # !!
-                                                                       # !!
-    cases_test_day = diagData$diagnosis_day + diagData$N_days_before,  # !!
-                                                                       # !!
-    # NEED A PRIOR ON THIS                                             # !!
-    cases_days_delay = diagData$days_delay,                            # !!
+    N_conf_cases = length(diagData$diagnosis_day),
+
+    #n days to model before first case
+    N_days = N_days,
+
+    # model days after (for things like death, hospitalizations)
+    N_days_extra = N_days_extra,
+
+    # maximum value for delay distribution
+    Max_delay = max(diagData$days_delay),
+
+    cases_test_day = diagData$diagnosis_day + diagData$N_days_before,
+
+    # NEED A PRIOR ON THIS
+    cases_days_delay = diagData$days_delay,
 
     ## Priors Parameters of random walk in log space <- new infections per day
     # mean of log daily infections on day 1
     # pri_log_new_inf_0_mu = -2,
- 
+
     # sd of log daily infections on day 1
     # pri_log_new_inf_0_sd = 2,
 
@@ -209,57 +209,17 @@ genData <- function(diagData)
     # gives rw momentum
     # pri_deriv2_log_new_inf_sd = 0.1,
 
-    spl_basis = spl_basis,                  # !!
-    n_spl_par = n_spl_par,                  # !!
-                                            # !!
-    # transitions                               
-    # pri_p_sym_if_inf_a = 69,                    
-    # pri_p_sym_if_inf_b = 31,                    
-    # pri_p_hos_if_sym_a = 31,                    
-    # pri_p_hos_if_sym_b = 69,                    
-    # pri_p_die_if_hos_a = 3,                     
-    # pri_p_die_if_hos_b = 97,                    
-    !!! priors_transitions(),
-                                            # !!
-    # poisson or negative binomial          # !!
-    nb_yes = 0,                             # !!
-    rw_yes = 0,                             # !!
+    spl_basis = spl_basis,
+    n_spl_par = n_spl_par,
 
-    # delay to progression
-    # inf_prg_delay_shap = 5.202,                                
-    # inf_prg_delay_rate = 0.946,                                
-    # sym_prg_delay_shap = 51.47,                                
-    # sym_prg_delay_rate = 4.679,                                
-    # hos_prg_delay_shap = 91.64,                                
-    # hos_prg_delay_rate = 10.41,                                
+    # poisson or negative binomial
+    nb_yes = 0,
+    rw_yes = 0,
+
+    !!! priors_transitions(),
     !!! priors_progression(),
-                                                                   
-    # delay to recovered                                           
-    # inf to res not well supported in data, guess:                
-    # assume mean 10 days, CI 5, 13                                
-    # based on 5 days inf -> sym, 5 days sym -> res (CI 2,8)       
-    # adjust later?                                                
-    # inf_res_delay_shap = 23.83,                                    
-    # inf_res_delay_rate = 2.383,                                    
-    # sym_res_delay_shap = 10.50,                                    
-    # sym_res_delay_rate = 2.099,                                    
-    # hos_res_delay_shap = 60.86,                                    
-    # hos_res_delay_rate = 3.567,                                    
     !!! priors_recovery(),
-                                                               
-    # report delay: assumed                                    
-    # pri_report_delay_shap = 7,                                 
-    # pri_report_delay_rate = 0.9,                               
     !!! priors_reporting_delay(),
-                                                               
-    # probability of diagnosis                                 
-    # assumed                                                  
-    # pri_p_diag_if_inf_a = 1,                                   
-    # pri_p_diag_if_inf_b = 99,                                  
-    # pri_p_diag_if_sym_a = 60,                                  
-    # pri_p_diag_if_sym_b = 40,                                  
-    # pri_p_diag_if_hos_a = 95,                                  
-    # pri_p_diag_if_hos_b = 5,
     !!! priors_diagnosis()
   )
 }
