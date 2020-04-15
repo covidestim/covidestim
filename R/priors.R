7d8e60d30847f597d8f8f0e0ad042e2ba109c7c9
Marcus Russi <marcusrussi@gmail.com>
2020-04-15 18:42:06 -0400

    Big documentation improvements and better OO structure

--- R/priors.R
+++ R/priors.R
@@ -1,21 +1,69 @@
+# Alias for making assertions on code
 att <- assertthat::assert_that
 
-splice_priors <- function(defaults, args)
+# Combines 'defaults' and 'args' together. If 'args'==list(), it is stripped
+# from the result. Any duplicate keys are resolved by selecting the last key
+# specified. The result has class 'modelconfig'
+splice_class <- function(defaults, args, class)
   structure(
     rlang::dots_list(
       !!! defaults,
       !!! args,
       .homonyms     = 'last',
       .ignore_empty = 'trailing'),
-    class='priors')
+    class=class)
+
+# Example model configuration object. It can be modified through overloading
+# the addition operator
+modelconfig <- function(...) {
+  config <- list(...)
+
+  # Declare that this is a class
+  structure(config, class='modelconfig')
+}
+
+# A list with arbitrary values, of class 'priors'
+priors <- function(...) structure(list(...), class='priors')
+
+# An overloaded addition operator, dispatched on the type of the lhs argument.
+# Flips the order of the arguments and calls 'modelconfig_add' to enable type
+# matching on the rhs operand.
+"+.modelconfig" <- function(a, b) {
+  # Dispatching on the type of 'b', which should be 'priors' for now.
+  modelconfig_add(b, a)
+}
+
+modelconfig_add <- function(rightside, leftside) UseMethod('modelconfig_add') 
+
+# Specialization for 'priors' classes. Splices in all the keys from a 'priors'
+# obj into a 'modelconfig' (leftside) object
+modelconfig_add.priors <- function(rightside, leftside)
+  rlang::dots_list(!!!leftside, !!!rightside, .homonyms='last')
+          
+# BUG: the following description needs information about what the defaults
+# were sourced from
 
 #' Priors for transitions
 #'
-#' Slightly longer description
+#' This function returns a keyed list of priors related to progression to
+#' various health states. Called with no arguments, the default values are
+#' returned. The following arguments can be passed to create different priors:
 #'
-#' @param ... Keys of the list!
+#' \code{inf_prg_delay_shap}
+#' \code{inf_prg_delay_rate}
+#' \code{sym_prg_delay_shap}
+#' \code{sym_prg_delay_rate}
+#' \code{hos_prg_delay_shap}
+#' \code{hos_prg_delay_rate}
+#' 
+#' Default values of these priors go here.
 #'
-#' @return An S3 object of class 'priors'
+#' @param ... A set of key-value pairs from those listed above. If no
+#'   arguments are passed, default values will be used
+#'
+#' @return An S3 object of class 'modelconfig'
+#' @examples
+#' myData <- defaultData() + priors_progression(inf_prg_delay_shap = 3.5)
 #' @export
 priors_transitions <- function(...) {
   args <- list(...)
@@ -29,18 +77,35 @@ priors_transitions <- function(...) {
     pri_p_die_if_hos_b = 97
   ) -> defaults
 
-  att(all(args %in% defaults))
+  att(all(names(args) %in% names(defaults)))
 
-  splice_priors(defaults, args)
+  splice_class(defaults, args, 'priors')
 }
+
+# BUG: the following description needs information about what the defaults
+# were sourced from
 
 #' Priors on delay to progression
 #'
-#' Slightly longer description
+#' This function returns a keyed list of priors related to progression to
+#' the infectious state. Called with no arguments, the default values are
+#' returned. The following arguments can be passed to create different priors:
 #'
-#' @param ... Keys of the list!
+#' \code{inf_prg_delay_shap}
+#' \code{inf_prg_delay_rate}
+#' \code{sym_prg_delay_shap}
+#' \code{sym_prg_delay_rate}
+#' \code{hos_prg_delay_shap}
+#' \code{hos_prg_delay_rate}
+#' 
+#' Default values of these priors 
 #'
-#' @return An S3 object of class 'priors'
+#' @param ... A set of key-value pairs from those listed above. If no
+#'   arguments are passed, default values will be used
+#'
+#' @return An S3 object of class 'modelconfig'
+#' @examples
+#' myData <- defaultData() + priors_progression(inf_prg_delay_shap = 3.5)
 #' @export
 priors_progression <- function(...) {
   args <- list(...)
@@ -54,24 +119,38 @@ priors_progression <- function(...) {
     hos_prg_delay_rate = 10.41
   ) -> defaults
 
-  att(all(args %in% defaults))
+  att(all(names(args) %in% names(defaults)))
 
-  splice_priors(defaults, args)
+  splice_class(defaults, args, 'priors')
 }
 
 # inf to res not well supported in data, guess:
 # assume mean 10 days, CI 5, 13
 # based on 5 days inf -> sym, 5 days sym -> res (CI 2,8)
 # adjust later?
-#
+# BUG: ^ This needs to be integrated into the documentation!
 
 #' Priors on delay to recovered
 #'
-#' Slightly longer description
+#' This function returns a keyed list of priors related to recovery from
+#' the infectious state. Called with no arguments, the default values are
+#' returned. The following arguments can be passed to create different priors:
 #'
-#' @param ... Keys of the list!
+#' \code{inf_res_delay_shap}
+#' \code{inf_res_delay_rate}
+#' \code{sym_res_delay_shap}
+#' \code{sym_res_delay_rate}
+#' \code{hos_res_delay_shap}
+#' \code{hos_res_delay_rate}
+#' 
+#' Default values of these priors 
 #'
-#' @return An S3 object of class 'priors'
+#' @param ... A set of key-value pairs from those listed above. If no
+#'   arguments are passed, default values will be used
+#'
+#' @return An S3 object of class 'modelconfig'
+#' @examples
+#' myData <- defaultData() + priors_recovery(inf_res_delay_shap = 20.12)
 #' @export
 priors_recovery <- function(...) {
   args <- list(...)
@@ -85,18 +164,29 @@ priors_recovery <- function(...) {
     hos_res_delay_rate = 3.567
   ) -> defaults
 
-  att(all(args %in% defaults))
+  att(all(names(args) %in% names(defaults)))
 
-  splice_priors(defaults, args)
+  splice_class(defaults, args, 'priors')
 }
 
-#' Priors on reporting delay: assumed
+#' Priors on reporting delay
 #'
-#' Slightly longer description
+#' This function returns a keyed list of priors related to reporting delay.
+#' Called with no arguments, the default values are returned. The following
+#' arguments can be passed to create different priors:
 #'
-#' @param ... Keys of the list!
+#' \code{pri_report_delay_shap}
+#' \code{pri_report_delay_rate}
 #'
-#' @return An S3 object of class 'priors'
+#' Default values should be explained here, as well as constraints on custom
+#' values. (Default values are assumed?)
+#'
+#' @param ... A set of key-value pairs from those listed above. If no
+#'   arguments are passed, default values will be used
+#'
+#' @return An S3 object of class 'modelconfig'
+#' @examples
+#' myData <- defaultData() + priors_reporting_delay(pri_report_delay_shap = 6)
 #' @export
 priors_reporting_delay <- function(...) {
   args <- list(...)
@@ -106,18 +196,34 @@ priors_reporting_delay <- function(...) {
     pri_report_delay_rate = 0.9
   ) -> defaults
 
-  att(all(args %in% defaults))
+  att(all(names(args) %in% names(defaults)))
 
-  splice_priors(defaults, args)
+  splice_class(defaults, args, 'priors')
 }
 
-#' Priors on probability of diagnosis (assumed)
+#' Priors on probability of diagnosis
 #'
-#' Slightly longer description
+#' This function returns a keyed list of priors related to probability of
+#' diagnosis.  Called with no arguments, the default values are returned. The
+#' following arguments can be passed to create different priors:
 #'
-#' @param ... Keys of the list!
+#' \code{pri_p_diag_if_inf_a}
+#' \code{pri_p_diag_if_inf_b}
+#' \code{pri_p_diag_if_sym_a}
+#' \code{pri_p_diag_if_sym_b}
+#' \code{pri_p_diag_if_hos_a}
+#' \code{pri_p_diag_if_hos_b}
 #'
-#' @return An S3 object of class 'priors'
+#' Default values should be explained here, as well as constraints on custom
+#' values. (Default values are assumed?)
+#'
+#' @param ... A set of key-value pairs from those listed above. If no
+#'   arguments are passed, default values will be used
+#'
+#' @return An S3 object of class 'modelconfig'
+#' @examples
+#' myData <- defaultData() + priors_diagnosis(pri_p_diag_if_inf_a = 1.2)
+#' @export
 priors_diagnosis <- function(...) {
   args <- list(...)
 
@@ -130,12 +236,11 @@ priors_diagnosis <- function(...) {
     pri_p_diag_if_hos_b = 5
   ) -> defaults
 
-  att(all(args %in% defaults))
+  att(all(names(args) %in% names(defaults)))
 
-  splice_priors(defaults, args)
+  splice_class(defaults, args, 'priors')
 }
 
-#' @export
 #' High level description of the function
 #'
 #' More extended description of the function
@@ -170,7 +275,7 @@ genData <- function(diagData)
   spl_basis <- as.matrix(as.data.frame(des_mat))
 
   # The first set of components of 'datList'
-  rlang::dots_list(
+  config <- rlang::dots_list(
     .homonyms = "error", # Ensure that no keys are entered twice
 
     N_conf_cases = length(diagData$diagnosis_day),
@@ -222,6 +327,8 @@ genData <- function(diagData)
     !!! priors_reporting_delay(),
     !!! priors_diagnosis()
   )
+
+  structure(config, class='modelconfig')
 }
 
 #' High level description of the function
@@ -239,5 +346,5 @@ genData <- function(diagData)
 #' @examples
 #' print(mtcars)
 #' @export
-defaultData <- function() genData(genFakeData())
+defaultData <- genData(genFakeData())
 
