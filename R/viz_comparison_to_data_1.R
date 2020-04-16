51eecdfcf85eb456a043a7443e12c2a187d81577
Marcus Russi <marcusrussi@gmail.com>
2020-04-16 01:50:24 -0400

    Un-broke the first vizualisation function

--- R/viz_comparison_to_data_1.R
+++ R/viz_comparison_to_data_1.R
@@ -1,12 +1,12 @@
-comparison_to_data_1 <- function(samps, datList) {
+comparison_to_data_1 <- function(samps, datList, diagData) {
   ########### Comparison to data ###########
-  pdfnam <- "Fig_COVID_nowcast_temp_4-7-2020.pdf"
+  pdfnam <- "covidcast_comparison_to_data_1.pdf"
   pdf(file = pdfnam, width = 9, height = 6.5)
 
   N_days <- datList[["N_days"]]
   N_days_tot <- datList[["N_days"]] + datList[["N_days_extra"]]
 
-  max_delay <- max(days_delay) + 1
+  max_delay <- max(diagData$days_delay) + 1
   cls <- colorRampPalette(c("red4", "red", "blue", "blue4"))(max_delay)
 
   otcm_mu <- apply(samps[["rep_tri_conf_cases_mu"]], 2:3, mean)
@@ -20,8 +20,8 @@ comparison_to_data_1 <- function(samps, datList) {
          otcm_mu[1:(N_days - i + 1), i],
          ylim = c(0,
                   max(c(otcm_mu[1:(N_days - i + 1), i],
-                        rep_tri_conf_cases[1:(N_days - i + 1), i]))) * 1.12,
-         xlim = c(N_days_before, N_days),
+                        diagData$rep_tri_conf_cases[1:(N_days - i + 1), i]))) * 1.12,
+         xlim = c(diagData$N_days_before, datList$N_days),
          type = "l",
          col = cls[i],
          axes = F,
@@ -32,9 +32,9 @@ comparison_to_data_1 <- function(samps, datList) {
     axis(1, las = 1, tcl = -0.07, mgp = c(3, 0.1, 0))
     box()
 
-    text(N_days_before,
+    text(diagData$N_days_before,
          max(c(otcm_mu[1:(N_days - i + 1), i],
-               rep_tri_conf_cases[1:(N_days - i + 1), i])) * 1.06,
+               diagData$rep_tri_conf_cases[1:(N_days - i + 1), i])) * 1.06,
          paste0(i - 1, " days delay"),
          pos = 4,
          offset = -0.2,
@@ -42,11 +42,11 @@ comparison_to_data_1 <- function(samps, datList) {
          cex = 1.15)
 
     lines(1:(N_days - i + 1),
-          rep_tri_conf_cases[1:(N_days - i + 1), i],
+          diagData$rep_tri_conf_cases[1:(N_days - i + 1), i],
           col = cls[i],
           lty = "12")
     points(1:(N_days - i + 1),
-           rep_tri_conf_cases[1:(N_days - i + 1), i],
+           diagData$rep_tri_conf_cases[1:(N_days - i + 1), i],
            col = cls[i],
            pch = 16,
            cex = 0.6)
@@ -54,5 +54,5 @@ comparison_to_data_1 <- function(samps, datList) {
   mtext("Case count (N)", 2, 1, outer = T)
   mtext("Fit to data: Case time-series, stratified by days delay (points = data, lines = model)",
         3, 1, outer = T)
-
+  dev.off()
 }
