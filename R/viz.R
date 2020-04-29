dbb9c8399619b7862b7b04030526f172f111d939
Marcus Russi <marcusrussi@gmail.com>
2020-04-29 01:19:27 -0400

    Changed viz.R to only call Mel's code

--- R/viz.R
+++ R/viz.R
@@ -4,11 +4,7 @@ viz.covidcast_result <- function(cc) {
 
   pdf()
 
-  viz_comparison_to_data_2(cc)
-  viz_comparison_to_data_2(cc)
-  viz_current_and_cumulative_outcomes(cc)
-  viz_incident_outcomes(cc)
-  viz_priors_vs_posteriors(cc)
+  viz_melanie(cc)
 
   dev.off()
 }
