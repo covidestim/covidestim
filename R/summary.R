7e898daa218adb6924c624770ed2a5f4b428343c
Marcus Russi <marcusrussi@gmail.com>
2020-05-29 08:40:12 -0400

    Simple version of summary() function

--- R/summary.R
+++ R/summary.R
@@ -3,13 +3,17 @@ summary.covidcast_result <- function(ccr, include.before = FALSE,
                                      include.RtEstim = TRUE) {
 
   att(identical(include.before, FALSE))
-  att(identical(include.RtEstim, FALSE))
+  att(identical(include.RtEstim, TRUE))
 
   c(
     "incidence.est" = "new_inf",
     "cases.fitted"  = "occur_cas",
-    "deaths.fitted" = "occur_die",
+    "deaths.fitted" = "occur_die"
   ) -> params
 
-  rstan::summary
+  rstan::summary(
+    ccr$result,
+    pars = "new_inf[1]",
+    probs = c(0.025, 0.5, 0.975)
+  )
 }
