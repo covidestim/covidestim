2fe3278cfa0064a1376d36e44446db6940585885
Marcus Russi <marcusrussi@gmail.com>
2020-09-15 14:39:08 -0400

    Update to example_nyc_data, 120 days now

--- data-raw/nyc_daily_counts.R
+++ data-raw/nyc_daily_counts.R
@@ -1,12 +1,14 @@
+library(magrittr)
+
 readr::read_csv(
   'data-raw/nyc_daily_counts.csv',
   col_types = readr:: cols(
-    date     = readr::col_date(format = "%m/%d/%y"),
+    date     = readr::col_date(format = "%m/%d/%Y"),
     .default = readr::col_number()
   )
 ) %>%
 tidyr::replace_na(
-  list(cases=0, hosps=0, deaths=0)
+  list(cases=0, deaths=0)
 ) -> nyc_data
 
-usethis::use_data(nyc_data, internal = TRUE)
+usethis::use_data(nyc_data, internal = TRUE, overwrite = TRUE)
