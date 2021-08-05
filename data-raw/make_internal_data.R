f1c15d8df727e566214eb86088a1da99081db49b
Marcus Russi <marcusrussi@gmail.com>
2021-08-05 09:25:25 -0400

    Removed unused internal object `ifr_prior`

--- data-raw/make_internal_data.R
+++ data-raw/make_internal_data.R
@@ -8,9 +8,8 @@ source('data-raw/generate_ifrs.R')
 
 ifr_state  <- ifrs$state
 ifr_county <- ifrs$county
-ifr_prior  <- ifrs$ifr_prior
 
 usethis::use_data(
-  ifr_state, ifr_county, ifr_prior, nyc_data, pop_state, pop_county,
+  ifr_state, ifr_county, nyc_data, pop_state, pop_county,
   internal = TRUE, overwrite = TRUE
 )
